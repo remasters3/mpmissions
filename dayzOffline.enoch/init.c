595164cfde541170622a0f42107582301601c1b6
Gerry <gerryfranchetti@gmail.com>
2024-02-24 21:32:32 +0000

    02/24/2024 21:32:32

--- dayzOffline.enoch/init.c
+++ dayzOffline.enoch/init.c
@@ -1,3 +1,24 @@
+static Object SpawnObject(string type, vector position, vector orientation, float scale = 1.0)
+{
+    Object obj = GetGame().CreateObjectEx(type, position, ECE_SETUP | ECE_UPDATEPATHGRAPH | ECE_CREATEPHYSICS);
+    if (!obj) {
+        Error("Failed to create object " + type);
+        return null;
+    }
+
+    obj.SetPosition(position);
+    obj.SetOrientation(orientation);
+    obj.SetOrientation(obj.GetOrientation());
+    obj.SetScale(scale);
+    obj.Update();
+	obj.SetAffectPathgraph(true, false);
+	if (obj.CanAffectPathgraph()) {
+        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(GetGame().UpdatePathgraphRegionByObject, 100, false, obj);
+    } 
+
+    return obj;
+}
+
 void main()
 {
 	//INIT ECONOMY--------------------------------------
@@ -7,7 +28,7 @@ void main()
 
 	//DATE RESET AFTER ECONOMY INIT-------------------------
 	int year, month, day, hour, minute;
-	int reset_month = 9, reset_day = 20;
+	int reset_month = 8, reset_day = 10;
 	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
 	if ((month == reset_month) && (day < reset_day))
@@ -28,6 +49,19 @@ void main()
 			}
 		}
 	}
+    SpawnObject("Land_Misc_Well_Pump_Yellow", "573.500549 351.346832 2742.822998", "-64.702881 0.000000 -0.000000", 0.999998);
+    SpawnObject("Land_Castle_Wall1_20", "603.335999 359.103851 2736.898438", "131.865509 -0.570536 2.188924", 0.999734);
+    SpawnObject("Land_Castle_Bastion", "618.598083 359.315857 2753.238770", "93.282326 0.000000 -0.000000", 0.999998);
+    SpawnObject("Land_Castle_Bastion", "586.700073 358.681519 2720.966797", "177.150284 0.000000 0.000000", 0.999998);
+    SpawnObject("Land_Castle_Bastion", "592.132935 358.982086 2781.154785", "-0.354077 0.000000 -0.000000", 0.999993);
+    SpawnObject("Land_Castle_Bastion", "560.495728 359.055176 2746.491455", "-87.383583 0.000000 -0.000000", 0.999995);
+    SpawnObject("Land_Castle_Bergfrit", "574.157776 364.090363 2735.145020", "126.000206 0.000000 -0.000000", 1);
+    SpawnObject("Land_Castle_Bergfrit", "603.290771 364.608795 2765.886719", "-47.950634 0.000000 -0.000000", 0.999997);
+    SpawnObject("Land_Castle_Stairs", "593.317444 354.760895 2765.894043", "44.999992 -0.000000 -0.000000", 1);
+    SpawnObject("Land_Castle_Stairs", "584.028992 354.253204 2736.413330", "-142.818466 -0.000000 -0.000000", 1);
+    SpawnObject("Land_Castle_Gate", "572.914307 354.272919 2767.389404", "-50.172634 0.000000 0.000000", 0.999994);
+    SpawnObject("Land_FuelStation_Feed_Enoch", "575.137756 351.337250 2741.212646", "32.041954 0.000000 -0.000000", 0.999997);
+    SpawnObject("Land_House_1W10_Brown", "598.162964 352.500214 2741.896484", "-47.153172 0.000000 0.000000", 0.999991);
 }
 
 class CustomMission: MissionServer
@@ -59,141 +93,26 @@ class CustomMission: MissionServer
 		ItemBase itemBs;
 		float rand;
 
-		player.RemoveAllItems();
-
-		int JKIndex = Math.RandomInt( 0, 4 );
-		string JacketArray[] = { "HikingJacket_Black", "HikingJacket_Blue", "HikingJacket_Green", "HikingJacket_Red" };
-		itemEnt = player.GetInventory().CreateInInventory( JacketArray[JKIndex] );
-
-		int PTIndex = Math.RandomInt( 0, 4 );
-		string PantsArray[] = { "CargoPants_Beige", "CargoPants_Black", "CargoPants_Green", "CargoPants_Grey" };
-		itemEnt = player.GetInventory().CreateInInventory( PantsArray[PTIndex] );
-
-		int FCIndex = Math.RandomInt( 0, 4 );
-		string FaceCoverArray[] = { "BalaclavaMask_Beige", "BalaclavaMask_Black", "BalaclavaMask_Blackskull", "BalaclavaMask_Green" };
-		itemEnt = player.GetInventory().CreateInInventory( FaceCoverArray[FCIndex] );
-
-		int BOIndex = Math.RandomInt( 0, 4 );
-		string BootsArray[] = { "HikingBootsLow_Beige", "HikingBootsLow_Black", "HikingBootsLow_Blue", "HikingBootsLow_Grey" };
-		itemEnt = player.GetInventory().CreateInInventory( BootsArray[BOIndex] );
-
-		int GLVIndex = Math.RandomInt( 0, 4 );
-		string GlovesArray[] = { "WorkingGloves_Beige", "WorkingGloves_Black", "WorkingGloves_Brown", "WorkingGloves_Yellow" };
-		itemEnt = player.GetInventory().CreateInInventory( GlovesArray[GLVIndex] );
-
-		int HATIndex = Math.RandomInt( 0, 4 );
-		string HatArray[] = { "RadarCap_Black", "RadarCap_Blue", "RadarCap_Brown", "RadarCap_Green" };
-		itemEnt = player.GetInventory().CreateInInventory( HatArray[HATIndex] );
-
-		int VSTIndex = Math.RandomInt( 0, 4 );
-                string VestArray[] = { "PressVest_Blue", "PressVest_LightBlue", "PressVest_Blue", "PressVest_LightBlue" };
-                itemEnt = player.GetInventory().CreateInInventory( VestArray[VSTIndex] );
-
-		int BAGIndex = Math.RandomInt( 0, 4 );
-		string BagArray[] = { "TaloonBag_Blue", "TaloonBag_Green", "TaloonBag_Orange", "TaloonBag_Orange" };
-                itemEnt = player.GetInventory().CreateInInventory( BagArray[BAGIndex] );
-
-		int BLTIndex = Math.RandomInt( 0, 4 );
-                string BeltArray[] = { "CivilianBelt", "CivilianBelt", "CivilianBelt", "CivilianBelt" };
-                itemEnt = player.GetInventory().CreateInInventory( BeltArray[BLTIndex] );
-
-		int ABIndex = Math.RandomInt( 0, 7 );
-                string ArmbandArray[] = { "Armband_Black", "Armband_Green", "Armband_Orange", "Armband_Blue", "Armband_Red", "Armband_White","Armband_Yellow" };
-                itemEnt = player.GetInventory().CreateInInventory( ArmbandArray[ABIndex] );
-
 		itemClothing = player.FindAttachmentBySlotName( "Body" );
 		if ( itemClothing )
 		{
-
-
+			SetRandomHealth( itemClothing );
+			
 			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
-			if ( Class.CastTo( itemBs, itemEnt ) )
-				itemBs.SetQuantity( 2 );
+			player.SetQuickBarEntityShortcut(itemEnt, 2);
 
 			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
 			int rndIndex = Math.RandomInt( 0, 4 );
 			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
-
-
-			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
-			if ( rand < 0.35 )
-				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
-			else if ( rand > 0.65 )
-				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
-			else
-				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
-
+			player.SetQuickBarEntityShortcut(itemEnt, 1);
 			SetRandomHealth( itemEnt );
 		}
-
+		
 		itemClothing = player.FindAttachmentBySlotName( "Legs" );
-		//if ( itemClothing )
-
+		if ( itemClothing )
+			SetRandomHealth( itemClothing );
+		
 		itemClothing = player.FindAttachmentBySlotName( "Feet" );
-
-		itemEnt = player.GetInventory().CreateInInventory("Compass");
-		itemEnt = player.GetInventory().CreateInInventory("ChernarusMap");
-		itemEnt = player.GetInventory().CreateInInventory("Headtorch_Black");
-
-		itemEnt = player.GetInventory().CreateInInventory("CombatKnife");
-		itemEnt = player.GetInventory().CreateInInventory("Matchbox");
-		itemEnt = player.GetInventory().CreateInInventory("Battery9V");
-		itemEnt = player.GetInventory().CreateInInventory("Battery9V");
-		itemEnt = player.GetInventory().CreateInInventory("DuctTape");
-		itemEnt = player.GetInventory().CreateInInventory("Whetstone");
-		itemEnt = player.GetInventory().CreateInInventory("CanOpener");
-		itemEnt = player.GetInventory().CreateInInventory("WoodAxe");
-		itemEnt = player.GetInventory().CreateInInventory("SewingKit");
-		itemEnt = player.GetInventory().CreateInInventory("LeatherSewingKit");
-
-		itemEnt = player.GetInventory().CreateInInventory("BandageDressing");
-		itemEnt = player.GetInventory().CreateInInventory("PurificationTablets");
-
-		itemEnt = player.GetInventory().CreateInInventory("SardinesCan");
-		itemEnt = player.GetInventory().CreateInInventory("BakedBeansCan");
-		itemEnt = player.GetInventory().CreateInInventory("SodaCan_Pipsi");
-		itemEnt = player.GetInventory().CreateInInventory("SodaCan_Spite");
-		itemEnt = player.GetInventory().CreateInInventory("TacticalBaconCan");
-        itemEnt = player.GetInventory().CreateInInventory("Canteen");
-        
-    
-        
-
-        array<string> MPFIVEArray = { "MP5K", "Mag_MP5_30Rnd", "Mag_MP5_30Rnd", "AmmoBox_9x19_25rnd", "AmmoBox_9x19_25rnd","AmmoBox_9x19_25rnd","PistolSuppressor","MP5k_StockBttstck","MP5_RailHndgrd","ACOGOptic" };
-        array<string> UMPArray = { "UMP45", "Mag_UMP_25Rnd", "Mag_UMP_25Rnd", "AmmoBox_45ACP_25rnd", "AmmoBox_45ACP_25rnd","AmmoBox_45ACP_25rnd","PistolSuppressor","ACOGOptic" };
-        array<string> CRSOArray = { "CZ61", "Mag_CZ61_20Rnd", "Mag_CZ61_20Rnd", "Mag_CZ61_20Rnd", "Mag_CZ61_20Rnd","Mag_CZ61_20Rnd","PistolSuppressor" };
-        
-        int gunz = Math.RandomInt( 0, 3 );
-        //int gunz = 2;
-        if ( gunz == 1 )
-        {
-            foreach(int i, string j: UMPArray)
-            {
-                itemEnt = player.GetInventory().CreateInInventory( UMPArray[i] );
-            }
-        }
-        else if ( gunz == 2 )
-        {
-            foreach(int k, string l: MPFIVEArray)
-            {
-                itemEnt = player.GetInventory().CreateInInventory( MPFIVEArray[k] );
-            }
-        }
-        else 
-        {
-            foreach(int a, string b: CRSOArray)
-            {
-                itemEnt = player.GetInventory().CreateInInventory( CRSOArray[a] );
-            }
-        }
-        
-        array<string> SideArmArray = { "PlateCarrierHolster","MKII", "Mag_MKII_10Rnd","Mag_MKII_10Rnd","AmmoBox_22_50Rnd" };
-        foreach(int sai, string san: SideArmArray)
-        {
-            itemEnt = player.GetInventory().CreateInInventory( SideArmArray[sai] );
-        }  
-        
-        
 	}
 };
 
